c222d33f036a935967cf61416f10dced15d402e8
Bhagyashree <bhagyashree9402@gmail.com>
2020-06-01 19:59:27 -0700

    did more things in testing

--- test/util.test.js
+++ test/util.test.js
@@ -1,4 +1,4 @@
-const { add } = require("../util/util");
+const { add, minus } = require("../util/util");
 describe("Add", () => {
     describe("success", () => {
         it("Should return the sum of 2 passed argumnets", () => {
@@ -9,6 +9,32 @@ describe("Add", () => {
         it("Should throw an error if the first argument is not a number", () => {
             expect(() => add("2", 2)).toThrowError()
         })
+        it("Should throw an error if the second argument is not a number", () => {
+            expect(() => add(2, "2")).toThrowError()
+        })
+        it("Should throw an error if no arguments were received", () => {
+            expect(() => add()).toThrowError()
+        })
     })
 
 });
+
+describe("Minus", () => {
+    describe("success", () => {
+        it("Should return the subtraction of 2 passed argumnets", () => {
+            expect(minus(4, 2)).toBe(2);
+        })
+    })
+    describe("Failure", () => {
+        it("Should throw an error if the first argument is not a number", () => {
+            expect(() => minus("4", 2)).toThrowError()
+        })
+        it("Should throw an error if the second argument is not a number", () => {
+            expect(() => minus(4, "2")).toThrowError()
+        })
+        it("Should throw an error if no arguments were received", () => {
+            expect(() => minus()).toThrowError()
+        })
+    })
+
+})
--- util/util.js
+++ util/util.js
@@ -1,7 +1,14 @@
 const add = (num1, num2) => {
     if (typeof num1 != "number") throw new Error("1st argument must be a number")
+    if (typeof num2 != "number") throw new Error("2st argument must be a number")
     return num1 + num2;
+};
+
+const minus = (num1, num2) => {
+    if (typeof num1 != "number") throw new Error("1st argument must be a number")
+    if (typeof num2 != "number") throw new Error("2st argument must be a number")
+    return num1 - num2;
 };
 //console.log(add(2, 2));
 
-module.exports = { add };
+module.exports = { add, minus };
