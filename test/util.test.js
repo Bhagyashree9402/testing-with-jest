90539464e25435a3963ba6227f26035417f1907c
Bhagyashree <bhagyashree9402@gmail.com>
2020-06-01 19:20:28 -0700

    did more things in testing

--- test/util.test.js
+++ test/util.test.js
@@ -5,5 +5,10 @@ describe("Add", () => {
             expect(add(2, 2)).toBe(4);
         })
     })
+    describe("Failure", () => {
+        it("Should throw an error if the first argument is not a number", () => {
+            expect(() => add("2", 2)).toThrowError()
+        })
+    })
 
 });
--- util/util.js
+++ util/util.js
@@ -1,6 +1,7 @@
 const add = (num1, num2) => {
+    if (typeof num1 != "number") throw new Error("1st argument must be a number")
     return num1 + num2;
 };
-console.log(add(2, 2));
+//console.log(add(2, 2));
 
 module.exports = { add };
